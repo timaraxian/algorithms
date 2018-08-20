e11f3a5c52ab7504e1040aac0b197386e63e4862
Timothy Thatcher <thatcher.tim.tt@gmail.com>
2018-08-20 23:11:20 +0200

    :white_check_mark: test: heaps

---
+++ heaps/heaps_test.go
@@ -0,0 +1,162 @@
+package heaps
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestNewHeap(t *testing.T) {
+	h := NewHeap()
+
+	exp := make([]int,0)
+
+	if !reflect.DeepEqual(exp, h.items) {
+		t.Errorf("%v != %v", exp, h.items)
+	}
+}
+
+func TestHeap_Insert(t *testing.T) {
+	h := NewHeap()
+
+	h.Insert(1)
+
+	exp := []int{1}
+	if !reflect.DeepEqual(exp, h.items) {
+		t.Errorf("%v != %v", exp, h.items)
+	}
+
+	h.Insert(5)
+
+	exp = []int{1,5}
+	if !reflect.DeepEqual(exp, h.items) {
+		t.Errorf("%v != %v", exp, h.items)
+	}
+
+	h.Insert(3)
+
+	exp = []int{1,5,3}
+	if !reflect.DeepEqual(exp, h.items) {
+		t.Errorf("%v != %v", exp, h.items)
+	}
+}
+
+func TestHeap_Insert2(t *testing.T) {
+	h := NewHeap()
+
+	h.Insert(1)
+	exp := []int{1}
+	if !reflect.DeepEqual(exp, h.items) {
+		t.Errorf("%v != %v", exp, h.items)
+	}
+
+	h.Insert(1)
+	exp = []int{1,1}
+	if !reflect.DeepEqual(exp, h.items) {
+		t.Errorf("%v != %v", exp, h.items)
+	}
+
+	h.Insert(13)
+	exp = []int{1,1,13}
+	if !reflect.DeepEqual(exp, h.items) {
+		t.Errorf("%v != %v", exp, h.items)
+	}
+
+	h.Insert(3)
+	exp = []int{1,1,13,3}
+	if !reflect.DeepEqual(exp, h.items) {
+		t.Errorf("%v != %v", exp, h.items)
+	}
+
+	h.Insert(8)
+	exp = []int{1,1,13,3,8}
+	if !reflect.DeepEqual(exp, h.items) {
+		t.Errorf("%v != %v", exp, h.items)
+	}
+
+	h.Insert(5)
+	exp = []int{1,1,5,3,8,13}
+	if !reflect.DeepEqual(exp, h.items) {
+		t.Errorf("%v != %v", exp, h.items)
+	}
+
+	h.Insert(2)
+	exp = []int{1,1,2,3,8,13,5}
+	if !reflect.DeepEqual(exp, h.items) {
+		t.Errorf("%v != %v", exp, h.items)
+	}
+
+	h.Insert(21)
+	exp = []int{1,1,2,3,8,13,5,21}
+	if !reflect.DeepEqual(exp, h.items) {
+		t.Errorf("%v != %v", exp, h.items)
+	}
+}
+
+func TestHeap_ExtractMin(t *testing.T) {
+	h := NewHeap()
+
+	h.Insert(1)
+	h.Insert(1)
+	h.Insert(13)
+	h.Insert(3)
+	h.Insert(8)
+	h.Insert(5)
+	h.Insert(2)
+	h.Insert(21)
+
+	min, ok := h.ExtractMin()
+	exp := 1
+	if min != exp {
+		t.Errorf("%v != %v", min, exp)
+	}
+	if !ok {
+		t.Errorf("ok != true")
+	}
+	
+	min, ok = h.ExtractMin()
+	exp = 1
+	if min != exp {
+		t.Errorf("%v != %v", min, exp)
+	}
+
+	min, ok = h.ExtractMin()
+	exp = 2
+	if min != exp {
+		t.Errorf("%v != %v", min, exp)
+	}
+
+	min, ok = h.ExtractMin()
+	exp = 3
+	if min != exp {
+		t.Errorf("%v != %v", min, exp)
+	}
+
+	min, ok = h.ExtractMin()
+	exp = 5
+	if min != exp {
+		t.Errorf("%v != %v", min, exp)
+	}
+
+	min, ok = h.ExtractMin()
+	exp = 8
+	if min != exp {
+		t.Errorf("%v != %v", min, exp)
+	}
+
+	min, ok = h.ExtractMin()
+	exp = 13
+	if min != exp {
+		t.Errorf("%v != %v", min, exp)
+	}
+
+	min, ok = h.ExtractMin()
+	exp = 21
+	if min != exp {
+		t.Errorf("%v != %v", min, exp)
+	}
+
+	min, ok = h.ExtractMin()
+	if ok {
+		t.Errorf("ok != false")
+	}
+}
